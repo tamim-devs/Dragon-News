1248f509fc7859c10752dfd143d4a0d3b71d9c7f
tamim-devs <tamimh0727@gmail.com>
2026-05-06 09:15:41 +0600

    base url

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: "https://dragon-news-virid.vercel.app"
 })
 
 export const { signIn, signUp, useSession } = createAuthClient()

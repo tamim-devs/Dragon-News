556d0edcd0873a16e3308138fa0e62928ef9fc06
tamim-devs <tamimh0727@gmail.com>
2026-05-06 09:18:05 +0600

    .env update

--- src/lib/data.js
+++ src/lib/data.js
@@ -8,7 +8,7 @@ export async function getNewsByCatgoriesId(category_id) {
  const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`);
  const data = await res.json()
  return data.data
- 
+  
 }
 export async function getNewsDetailsById(news_id) {
   const res = await fetch(
